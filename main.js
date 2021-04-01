32685c881d99f20dff78da639e58d5eb4d8c478d
Yuzunoha <studysteadystandby@gmail.com>
2021-04-01 18:34:12 +0900

    押された

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
 'use strict';
 
-const p1 = document.getElementById('p1');
-
-const p = (arg) => {
-  p1.innerHTML += JSON.stringify(arg) + '<br>';
+const btnRead = document.getElementById('btnRead');
+btnRead.onclick = () => {
+  alert('押された');
 };
-
-const url = 'https://www.jma.go.jp/bosai/forecast/data/overview_week/130000.json';
-
-const asyncExtractStatusAndJson = async (res) => ({
-  status: res.status,
-  json: await res.json(),
-});
-
-const getStatusCodeAndJson = (url) => fetch(url).then(asyncExtractStatusAndJson);
-
-const main = async () => {
-  const result = await getStatusCodeAndJson(url);
-
-  console.log(result);
-  p(result);
-};
-
-main();
